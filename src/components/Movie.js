32d5259e5aaefd4eec6082f9a9bace3d6998df4b
unknown <lsc051790@naver.com>
2021-12-01 22:38:50 +0900

    .

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,25 @@
 import PropTypes from "prop-types"
 import {Link} from "react-router-dom"
+import styles from "../Movie.module.css"
 
 const Movie=({id,cover,title,summary,genres})=>{
     return (
-        <li>
-            <img src={cover} alt={title}/>
-            <h2>
-                <Link to={`/movie/${id}`}>{title}</Link>
-            </h2>
-            <p>{summary}</p>
-            <ul>
-                {genres.map(genre=><li key={genre}>{genre}</li>)}
-            </ul>
-        </li>
+        <div className={styles.movieDiv}>
+            <div className={styles.imgDiv}>
+                <img src={cover} alt={title} className={styles.cover}/>
+            </div>
+            <div className={styles.textDiv}>
+                <h4 className={styles.title}>
+                    <Link to={`/movie/${id}`}>{title}</Link>
+                </h4>
+                <ul className={styles.genreUl}>
+                    {genres.map(genre=><li className={styles.genreLi} key={genre}>{genre}</li>)}
+                </ul>
+                <div className={styles.summaryDiv}>
+                    <h5>{summary}</h5>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,12 @@
 import { useEffect,useState } from "react"
 import Movie from "../components/Movie"
+import styles from "../Home.module.css"
 
 const Home=()=>{
     const [loading,setLoading]=useState(true)
     const [movies,setMovies]=useState([])
     const getMovies=async ()=>{
-        const response=await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year')
+        const response=await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
         const json=await response.json()
         setMovies(json.data.movies)
         setLoading(false)
@@ -16,15 +17,16 @@ const Home=()=>{
     },[])
 
     return (
-        <div>
-            {loading ? <h1>loading...</h1> :
-            <ul>
+        <div className={styles.homeDiv}>
+            {loading ? 
+            <div className={styles.loading}>loading...</div> :
+            <>
               {movies.map(movie=>{
                 return (
                     <Movie key={movie.id} id={movie.id} cover={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />    
                 )
               })}  
-            </ul>
+            </>
             }
         </div>
     )
